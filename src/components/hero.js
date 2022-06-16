4eae9fcb26f8e8d301432d7b4524007706c590c1
RensVlooswijk <rensvlooswijk@gmail.com>
2022-06-16 13:25:04 +0200

    Update hero.js

--- src/components/hero.js
+++ src/components/hero.js
@@ -6,9 +6,9 @@ const logo = "https://i.ibb.co/T0SSqgt/Stokers-Logo-No-Background.png";
 const Hero = () => (
   <div className="text-center hero">
     <img className="mb-3 app-logo" src={logo} alt="React logo" width="120" />
-        <h1 className="mb-4" styles={{color: 'green'}}>Stokers Website</h1>
-    <p className="lead" styles={{color: 'yellow'}}>Welkom op de stokers website,</p>
-           <p styles={{color: 'red'}}>Hier kun je zien welke activiteiten allemaal geplanned staan en wat we allemaal al hebben gedaan!</p>  
+        <h1 className="mb-4" styles={{backgroundcolor: 'green'}}>Stokers Website</h1>
+    <p className="lead" styles={{backgroundcolor: 'yellow'}}>Welkom op de stokers website,</p>
+           <p styles={{backgroundcolor: 'red'}}>Hier kun je zien welke activiteiten allemaal geplanned staan en wat we allemaal al hebben gedaan!</p>  
         
     </div>);
 
